29b40ba932fb2a4dc634177317a10300e46fb4fa
Fodor Zsolt <fodorzsolt98@gmail.com>
2026-03-28 00:30:22 +0100

    Make website even better

--- static/write_message_for_table.js
+++ static/write_message_for_table.js
@@ -1,3 +1,6 @@
+let fault_counter = 0;
+let fault_list = ["Egy számot írj be, ez nem az!", "Ez még mindig nem egy szám!", "Nem ez még továbbra sem szám!", "Kezdem reménytelennek érezni a helyzetet...", "Nem még mindig nem szám. Kicsit sok lett a pia?", "Szerintem inkább hagyjuk, menj inkább táncolni!"]
+
 function write_message_for_table()
 {
     let table_num_field = document.getElementById("table_num");
@@ -13,9 +16,14 @@ function write_message_for_table()
         {
             table_message_field.innerHTML = "<p>Not Hello bello</p>";
         }
+        fault_counter = 0;
     }
     else
     {
-        table_message_field.innerHTML = "<p>Not even a nummber man!</p>";
+        table_message_field.innerHTML = "<p>" + fault_list[fault_counter] + "</p>";
+        if(fault_counter < 5)
+        {
+            fault_counter++;
+        }
     }
 }
